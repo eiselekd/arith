32dcfef3e7243119493ca1fcf01e374ce9c21f53
Konrad Eisele <eiselekd@gmail.com>
2017-10-22 14:11:35 +0000

    all

--- cla.pl
+++ cla.pl
@@ -1,33 +1,54 @@
 #!/usr/bin/perl
 
-@a=(0 x 64);
-@b=(0 x 64);
+@a=();
+@b=();
+@s=();
+
 $cin = 0;
-@p = ([] x 64);
-@g = ([] x 64);
-@g = ([] x 64);
-@c = ([] x 64);
+@p = ();
+@g = ();
+@c = ();
+
 @f = ();
 
-sub _next($i,$r) {
-    my $idx = scalar($$r[$i]);
+for ($i = 0; $i < 64; $i++) {
+    push(@a,0);
+    push(@b,0);
+    push(@c,0);
+    push(@s,0);
+    push(@p,[]);
+    push(@g,[]);
+}
+push(@c,0);
+
+sub _next {
+    my ($i,$r) = @_;
+    my $idx = scalar(@{$$r[$i]});
     push(@{$$r[$i]},0);
     return \$$r[$i][$idx];
 }
+sub next_g { my ($i) = @_; return _next($i,\@g);}
+sub next_p { my ($i) = @_; return _next($i,\@p);}
 
-sub next_g($i) { return _next($i,\@g);}
-sub next_p($i) { return _next($i,\@p);}
-sub next_c($i) { return _next($i,\@c);}
-
-sub first_c($i) {
+sub _last {
+    my ($i,$r) = @_;
     return \$cin if ($i < 0);
-    return \$c[$i][0]
+    my $idx = scalar(@{$$r[$i]})-1;
+    return \$$r[$i][$idx];
 }
+sub last_g { my ($i) = @_; return _last($i,\@g);}
+sub last_p { my ($i) = @_; return _last($i,\@p);}
 
-sub digit($v) { return $v > 0 ? 1 : 0; }
+sub nth_g { my ($i,$d) = @_; return \$g[$i][$d];}
+sub nth_p { my ($i,$d) = @_; return \$p[$i][$d];}
+
+sub c_pos { my ($i) = @_; return \$c[$i];}
+
+sub digit { my ($v) = @_; return $v > 0 ? 1 : 0; }
 
 # generate first stage propagate and generate
-sub gen_pg_0($i) {
+sub gen_pg_0 {
+    my ($i) = @_; 
     my $a0 = \$a[$i];
     my $b0 = \$b[$i];
     my $g0 = next_g($i);
@@ -42,24 +63,27 @@ for ($i = 0; $i < 64; $i++) {
     gen_pg_0($i);
 }
 
+
 # combine propagate and generate in blocks
-sub gen_pg($i,$l) {
-    my $g0 = last_g($i+0*$l);
-    my $g1 = last_g($i+1*$l);
-    my $g2 = last_g($i+2*$l);
-    my $g3 = last_g($i+3*$l);
-    my $p0 = last_p($i+0*$l);
-    my $p1 = last_p($i+1*$l);
-    my $p2 = last_p($i+2*$l);
-    my $p3 = last_p($i+3*$l);
-    my $g = next_g($i);
-    my $p = next_p($i);
+sub gen_pg {
+    
+    my ($i,$l) = @_; 
+    my $g0 = last_g($i+1*$l-1);
+    my $g1 = last_g($i+2*$l-1);
+    my $g2 = last_g($i+3*$l-1);
+    my $g3 = last_g($i+4*$l-1);
+    
+    my $p0 = last_p($i+1*$l-1);
+    my $p1 = last_p($i+2*$l-1);
+    my $p2 = last_p($i+3*$l-1);
+    my $p3 = last_p($i+4*$l-1);
+    
+    my $g = next_g($i+4*$l-1);
+    my $p = next_p($i+4*$l-1);
     
     push(@f,sub {
-	
 	$$g = digit($$g3+($$g2*$$p3)+($$g1*$$p2*$$p3)+($$g0*$$p1*$$p2*$$p3));
 	$$p = digit($$p0*$$p1*$$p2*$$p3);
-	
 	return ($$p,$$g);
 	 });
 }
@@ -77,56 +101,59 @@ for ($i = 0; $i < 64/64; $i++) {
     gen_pg($i*64,16);
 }
 
-sub gen_c($i,$l) {
-    my $g0 = last_g($i+0*$l);
-    my $g1 = last_g($i+1*$l);
-    my $g2 = last_g($i+2*$l);
-    my $g3 = last_g($i+3*$l);
+sub gen_c {
+    my ($i,$l,$d) = @_; 
+    my $g0 = nth_g($i+1*$l-1,$d);
+    my $g1 = nth_g($i+2*$l-1,$d);
+    my $g2 = nth_g($i+3*$l-1,$d);
+    my $g3 = nth_g($i+4*$l-1,$d);
     
-    my $p0 = last_p($i+0*$l);
-    my $p1 = last_p($i+1*$l);
-    my $p2 = last_p($i+2*$l);
-    my $p3 = last_p($i+3*$l);
+    my $p0 = nth_p($i+1*$l-1,$d);
+    my $p1 = nth_p($i+2*$l-1,$d);
+    my $p2 = nth_p($i+3*$l-1,$d);
+    my $p3 = nth_p($i+4*$l-1,$d);
 
-    my $cin0 = first_c($i+0*$l-1);
-    my $c0   =  next_c($i+1*$l-1);
-    my $c1   =  next_c($i+2*$l-1);
-    my $c2   =  next_c($i+3*$l-1);
-    my $c3   =  next_c($i+4*$l-1);
+    my $gin =  c_pos($i);
+    
+    my $c1  =  c_pos($i+1*$l);
+    my $c2  =  c_pos($i+2*$l);
+    my $c3  =  c_pos($i+3*$l);
     
     push(@f,sub {
-
-       #$$c0   = digit($$cin0);
-	$$c1   = digit($$cin0*$$p0           +$$g0);
-	$$c2   = digit($$cin0*$$p0*$$p1      +$$g0*$$p0      + $$g1);
-	$$c3   = digit($$cin0*$$p0*$$p1*$$p2 +$$g0*$$p1*$$p2 + $$g1*$$p2 + $$g2);
 	
+	$$c1   = digit($$gin*$$p0                +$$g0);
+	$$c2   = digit($$gin*$$p0*$$p1           +$$g0*$$p1      + $$g1);
+	$$c3   = digit($$gin*$$p0*$$p1*$$p2      +$$g0*$$p1*$$p2 + $$g1*$$p2 + $$g2);
+
 	 });
 }
 
+
+
 # define 3 carries
 for ($i = 0; $i < 64/64; $i++) {
-    gen_c($i*64,16);
+    gen_c($i*64,16,2);
 }
 
 # define 3*4 carries
 for ($i = 0; $i < 64/16; $i++) {
-    gen_c($i*16,4);
+    gen_c($i*16,4,1);
 }
 
 # define 3*16 carries
 for ($i = 0; $i < 64/4; $i++) {
-    gen_c($i*4,1);
+    gen_c($i*4,1,0);
 }
 
 # generate final sum
-sub gen_sum_0($i) {
+sub gen_sum_0 {
+    my ($i) = @_; 
     my $a0 = \$a[$i];
     my $b0 = \$b[$i];
-    my $s0 = \$b[$i];
-    my $c0 = first_c($i-1);
+    my $c0 = c_pos($i);
+    my $s0 = \$s[$i];
     push(@f,sub {
-	$$s0 = digit($$a0 + $$b0 + $c0);
+	$$s0 = ($$a0 + $$b0 + $$c0) & 1;
 	 });
 }    
 
@@ -137,8 +164,41 @@ for ($i = 0; $i < 64; $i++) {
 
 
 
-foreach my $f (@f) {
-    $$f();
+# test cla adder
+sub test_sum {
+    my ($a,$b,$cin) = @_;
+    
+    for (my $i = 0; $i < 64; $i++) {
+	$a[$i] = 0;
+	$b[$i] = 0;
+    }
+    
+    # init with 32 bit only
+    for (my $i = 0; $i < 32; $i++) {
+	$a[$i] = ($a & (1<<$i)) ? 1 : 0;
+	$b[$i] = ($b & (1<<$i)) ? 1 : 0;
+    }
+    
+    foreach my $f (@f) {
+	$f->();
+    }
+    
+    my $sum = 0;
+    for (my $i = 0; $i < 32; $i++) {
+	$sum |= ($s[$i] << $i);
+    }
+    
+    printf("sum: 0x%x\n", $sum);
+    return $sum;
 }
 
+while (1) {
+    $a = int(rand(0x7ffffff));
+    $b = int(rand(0x7ffffff));
+    $cin = 0;
+    my $sum = test_sum($a, $b, $cin);
+    if ($a + $b != $sum) {
+	die("Error\n");
+    } 
+}
 
